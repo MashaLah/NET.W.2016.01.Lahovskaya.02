890337e6c43c883e46c08e27a5da13a60dbfdc4c
MashaLah <lahoska91@mail.ru>
2016-10-24 18:20:26 +0300

    Check.

--- Task3UI/Program.cs
+++ Task3UI/Program.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3;
 
 namespace Task3UI
 {
@@ -13,7 +14,17 @@ namespace Task3UI
     {
         static void Main(string[] args)
         {
-
+            int a = InsertBytes.Insert(8, 15, 0, 0);
+            Console.WriteLine(a);
+            int b = InsertBytes.Insert(0, 15, 30, 30);
+            Console.WriteLine(b);
+            int c = InsertBytes.Insert(0, 15, 0, 30);
+            Console.WriteLine(c);
+            int d = InsertBytes.Insert(15, -15, 0, 4);
+            Console.WriteLine(d);
+            int e = InsertBytes.Insert(15, int.MaxValue, 3, 5);
+            Console.WriteLine(e);
+            Console.ReadLine();
         }
     }
 }
