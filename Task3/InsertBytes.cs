5004ae9264dd70725a81bae2d9579cede42a3895
MashaLah <lahoska91@mail.ru>
2016-10-24 18:01:24 +0300

    Add task3

---
+++ Task3/InsertBytes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class InsertBytes
+    {
+        public static int Insert(int firstNumber, int secondNumber, int firstIndex, int secondIndex)
+        {
+            BitArray arrFirst = new BitArray(new int[] { firstNumber });
+            BitArray arrSecond = new BitArray(new int[] { secondNumber });
+            for (int j = 0, i = firstIndex; (j < 1 + secondIndex - firstIndex) && (i <= secondIndex); j++, i++)
+            {
+                if (arrFirst[i] == true || arrSecond[j] == true) arrFirst[i] = true;
+            }
+        }
+    }
+}
---
+++ Task3UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
