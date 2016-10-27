762f7afaa8d4f68dfdb9f80f4774b804843631c9
MashaLah <lahoska91@mail.ru>
2016-10-27 17:04:18 +0300

    Add Task2.Tests

---
+++ Task2.Tests/ConcatedStringTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2;
+
+namespace Task2.Tests
+{
+    [TestClass]
+    public class ConcatedStringTests
+    {
+        /// <summary>
+        /// A test for MakeString() with valid strings.
+        /// </summary>
+        [TestMethod]
+        public void MakeString_ValidStrings_ExpectedString()
+        {
+            //arrange
+            string firstStr = "xyaabbbccccdefww";
+            string secondStr = "xxxxyyyyabklmopq";
+            string expected = "abcdefklmopqwxy";
+            //act
+            string actual = ConcatedString.MakeString(firstStr,secondStr);
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        /// <summary>
+        /// A test for MakeString() with one string is null.
+        /// </summary>
+        [TestMethod]
+        public void MakeString_StringIsNull_Message()
+        {
+            //arrange
+            string firstStr = "xyaabbbccccdefww";
+            string secondStr = null;
+            string expected = "Input string is null or contains not only letters.";
+            //act
+            string actual = ConcatedString.MakeString(firstStr, secondStr);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// A test for MakeString() with one string contains not only letters.
+        /// </summary>
+        [TestMethod]
+        public void MakeString_StringContainsNotOnlyLetters_Message()
+        {
+            //arrange
+            string firstStr = "xyaabbbccccdefww";
+            string secondStr = "123";
+            string expected = "Input string is null or contains not only letters.";
+            //act
+            string actual = ConcatedString.MakeString(firstStr, secondStr);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
